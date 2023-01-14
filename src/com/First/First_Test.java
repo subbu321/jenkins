89d70decdc7fe80e47f2868e61286c54b1da3119
subbuhtml <subbumonster7@gmail.com>
2023-01-14 11:57:56 +0530

    jenkins-integeration

---
+++ src/com/First/First_Test.java
@@ -0,0 +1,21 @@
+package com.First;
+
+import org.testng.annotations.Test;
+
+public class First_Test {
+	@Test
+	public void test1() {
+		System.out.println("this is test1");
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("this is test2");
+	}
+
+	@Test
+	public void test3() {
+		System.out.println("this is test3");
+	}
+	
+}
